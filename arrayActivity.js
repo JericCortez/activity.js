39859b063381c6ad4975357370312f1e73b6b98e
JericCortez <jericayson2@gmail.com>
2024-10-17 11:46:20 +0000

    initial

---
+++ arrayActivity.js
@@ -0,0 +1,12 @@
+let favSingers = ["John Mayer", "Adele", "Bruno Mars"];
+
+console.log(favSingers[0]);  // Output: John Mayer
+
+let favNumbers = [7, 14, 21, 28];
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]);  // Output: string
+console.log(mixedArr[1]);  // Output: ["otherarray"]
+console.log(mixedArr[2]);  // Output: 123
+console.log(mixedArr[3]);  // Output: true
